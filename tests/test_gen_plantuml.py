c00acc34b1155b3c7aab064eeca00a53372f760d
Hans Jang <hsjang8848@gmail.com>
2019-01-22 23:58:54 +1100

    Fix failing tests

--- tests/test_gen_plantuml.py
+++ tests/test_gen_plantuml.py
@@ -8,7 +8,7 @@ def check(tmpdir):
     def fn(sd_func, exp_lines):
         exp_lines = '@startuml' + exp_lines + '@enduml\n'
         sd_context = sd.parse(sd_func)
-        puml_file = gen_plantuml.generate(tmpdir, 'test', sd_context)[0]
+        puml_file = gen_plantuml.generate(str(tmpdir), 'test', sd_context)[0]
         with open(puml_file, 'rt') as f:
             lines = f.read()
         assert lines == exp_lines
