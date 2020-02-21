d721652bce2757b635e179b3cb80c33d467cb1d6
Scott Ouellette <5629547+scottx611x@users.noreply.github.com>
2020-02-21 13:52:57 -0500

    Specify required `url` arg to PlantUML constructor

--- napkin/gen_plantuml_png.py
+++ napkin/gen_plantuml_png.py
@@ -16,6 +16,6 @@ def generate(diagram_name, output_dir, sd_context):
                                             output_dir, sd_context)
     puml_path = generated_files[0]
     png_path = os.path.join(output_dir, diagram_name + '.png')
-    if plantuml.PlantUML().processes_file(puml_path, png_path):
+    if plantuml.PlantUML(plantuml.SERVER_URL).processes_file(puml_path, png_path):
         generated_files.append(png_path)
     return generated_files
