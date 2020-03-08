35ff70451a5d20449633f5b377dedb7ab263b970
Hans Jang <hsjang8848@gmail.com>
2020-03-08 16:42:57 +1100

    Tag version with 0.5.3.3

--- napkin/__init__.py
+++ napkin/__init__.py
@@ -3,7 +3,7 @@ import importlib
 import collections
 from . import sd
 
-__version__ = '0.5.3.2'
+__version__ = '0.5.3.3'
 
 # Name and description.
 # Note that the name should match to the module name, gen_<name>.
